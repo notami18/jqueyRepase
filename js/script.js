6f6bcf94087c9dc7b5775a7fdd81410ef762aa16
Edwin Roman <eroman@coopebombas.com>
2018-05-30 15:09:54 -0500

    Cambios notorios

--- js/script.js
+++ js/script.js
@@ -19,19 +19,64 @@
         }
     ];
 
-    newFunction(data);
+    MyFunction(data);
     
 })();
 
-function newFunction(data) {
-    $("#id").text(data[0].Id);
-    $("#Name").text(data[0].Name);
-    $("#Apellido").text(data[0].Apellido);
-    $("#Edad").text(data[0].Edad);
-
+function MyFunction(data) {
     $.each(data, function(key, attr){
-        $("#headingOne").append();
+        $("#Hola").append('<div class="card-header" id="headingOne">'+
+        '<h5 class="mb-0">'+
+        '<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse'+attr.Id+'" aria-controls="collapse'+attr.Id+'">'+
+          '00'+attr.Id+''+
+          '</button>'+
+          '<button class="btn btn-primary float-sm-right">Editar</button>'+
+          '<button class="btn btn-danger float-sm-right">Eliminar</button>'+
+        '</h5>'+
+      '</div>'+
+      '<div id="collapse'+attr.Id+'" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">'+
+      '<div class="card-body">'+
+        '<h3 class="card-title">Hola como estas?</h3>'+
+        '<dl class="dl-horizontal">'+
+          '<dt>ID</dt>'+
+          '<dd>'+attr.Id+'</dd>'+
+          '<dt>Nombre</dt>'+
+          '<dd>'+attr.Name+'</dd>'+
+          '<dt>Apellido</dt>'+
+          '<dd>'+attr.Apellido+'</dd>'+
+          '<dt>Edad</dt>'+
+          '<dd>'+attr.Edad+'</dd>'+
+        '</dl>'+
+      '</div>'+
+    '</div>');
+        
         console.log(attr);
     });
+
+    /*var i;
+    for (i = 0; i < data.length; i++) {
+        $("#Hola").append('<div class="card-header" id="headingOne">'+
+        '<h5 class="mb-0">'+
+        '<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse'+data[i].Id+'" aria-expanded="true" aria-controls="collapse'+data[i].Id+'">'+
+          '00'+data[i].Id+''+
+          '</button>'+
+        '</h5>'+
+      '</div>'+
+      '<div id="collapse'+data[i].Id+'" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">'+
+      '<div class="card-body">'+
+        '<h3 class="card-title">Hola como estas?</h3>'+
+        '<dl class="table-hover">'+
+          '<dt>ID</dt>'+
+          '<dd id="'+data[i].Id+'"></dd>'+
+          '<dt>Nombre</dt>'+
+          '<dd id="'+data[i].Name+'"></dd>'+
+          '<dt>Apellido</dt>'+
+          '<dd id="'+data[i].Apellido+'"></dd>'+
+          '<dt>Edad</dt>'+
+          '<dd id="'+ data[i].Edad+'"></dd>'+
+        '</dl>'+
+      '</div>'+
+    '</div>');
+    }*/
     
 }
